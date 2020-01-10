566941da483dadd0a1b6b55bfafa178657fdf500
Benoit Perrot <benoit.perrot@algolia.com>
2020-01-10 18:02:54 +0100

    generator: Generate fields' documentation

--- src/generator/python/generator.py
+++ src/generator/python/generator.py
@@ -30,10 +30,24 @@ def camel_case_to_snake_case(s):
 
 python_keywords = ['continue', 'except', 'from']
 python_builtins = ['type', 'exec', 'range', 'object', 'min', 'max']
+max_line_len = 80
 
 
-def generate_field_name(f: Field):
-    return f.name + '_' if f.name in python_keywords + python_builtins else camel_case_to_snake_case(f.name)
+def generate_field_name_and_doc(f: Field):
+    field_name = f.name + '_' if f.name in python_keywords + python_builtins else camel_case_to_snake_case(f.name)
+    indent = 2 * 4 * ' '
+    line = indent + ':param ' + field_name + ':'
+    indent += 7 * ' '
+    lines = []
+    for w in ' '.join(x for x in f.desc if not x.startswith('+')).split():
+        if len(line) + 1 + len(w) < max_line_len:
+            line += ' '
+        else:
+            lines.append(line)
+            line = indent
+        line += w
+    lines.append(line)
+    return field_name, '\n'.join(lines)
 
 
 def generate_field_type_name(f: Field):
@@ -59,29 +73,35 @@ def generate_field_type_name(f: Field):
 
 def generate_ctor(msg: Message):
     import_statements = set()
+    ctor_args_doc = []
     ctor_args = ['self']
     ctor_opt_args = []
     ctor_attrs_init = []
     for f in msg.fields:
-        field_name = generate_field_name(f)
+        field_name, field_doc = generate_field_name_and_doc(f)
+        ctor_args_doc.append(field_doc)
         field_type_name, field_import_statements = generate_field_type_name(f)
         import_statements.update(field_import_statements)
         (ctor_opt_args if f.is_optional else ctor_args).append(field_name + ': ' + field_type_name)
         ctor_attrs_init.append('self.' + field_name + ' = ' + field_name)
-    return import_statements, ctor_args + [a + ' = None' for a in ctor_opt_args], ctor_attrs_init
+    return import_statements, ctor_args_doc, ctor_args + [a + ' = None' for a in ctor_opt_args], ctor_attrs_init
 
 
 def generate_class(msg: Message):
-    import_statements, ctor_args, ctor_attrs_init = generate_ctor(msg)
+    import_statements, ctor_args_doc, ctor_args, ctor_attrs_init = generate_ctor(msg)
     return '''\
 {imports}\
 class {class_name}:
     def __init__({ctor_args}
                  ):
+        """
+        {ctor_doc}
+        """
         {ctor_body}
 '''.format(
         imports='\n'.join(sorted(import_statements)) + ('\n\n\n' if 0 < len(import_statements) else ''),
         class_name=msg.name,
         ctor_args=(',\n' + ' ' * 17).join(ctor_args),
+        ctor_doc=''.join('\n' + a for a in ctor_args_doc),
         ctor_body=('\n' + ' ' * 8).join(ctor_attrs_init) if 0 < len(ctor_attrs_init) else 'pass'
     )
